a151c5cb050f8571f21c10a58029613c8cb24f06
Jesús Enrique Rascón <blograso@gmail.com>
2022-12-17 13:57:57 +0100

    yeah

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -4,23 +4,23 @@ export async function load({ fetch }) {
 
 	let paradas = data.paradas.map((p) => p.idParada);
 
-	let urlHorarios = paradas.map(
-		(p) => 'http://api.ctan.es/v1/Consorcios/3/paradas/' + p + '/servicios?horaIni='
+	let urlHorarios = paradas.map((p) =>
+		fetch('http://api.ctan.es/v1/Consorcios/3/paradas/' + p + '/servicios?horaIni=')
 	);
 
-	let top2horas = urlHorarios.map((url) => {
-		// figure out how to fetch lots of urls on a loop or map
-		let horaInfo = fetch(url);
+	let promiseHorarios = await Promise.all(urlHorarios);
 
-		let horas = horaInfo.json();
+	// let top2horas = urlHorarios.map((url) => {
+	// 	// figure out how to fetch lots of urls on a loop or map
+	// 	let horaInfo = fetch(url);
 
-		return {
-			siguiente: horas[0].servicio,
-			despues: horas[1].servicio
-		};
-	});
+	// 	let horas = horaInfo.json();
 
-	console.log(top2horas);
+	// 	return {
+	// 		siguiente: horas[0].servicio,
+	// 		despues: horas[1].servicio
+	// 	};
+	// });
 
 	return data;
 }
