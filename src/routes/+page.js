ee0fb04d5402fb8747273bbc284bcc8e12b86c0a
Jesús Rascón <blograso@gmail.com>
2022-12-25 18:26:00 +0100

    yea

--- src/routes/+page.js
+++ src/routes/+page.js
@@ -6,9 +6,9 @@ async function fetchData(fetch) {
 	let paradasUnicas = paradas.paradas.filter((p) => p.sentido == '1');
 	let idParadas = paradasUnicas.map((p) => p.idParada);
 
-	let urlHorarios = idParadas.map((p) =>
-		fetch('http://api.ctan.es/v1/Consorcios/3/paradas/' + p + '/servicios?horaIni=')
-	);
+	// let urlHorarios = idParadas.map((p) =>
+	// 	fetch('http://api.ctan.es/v1/Consorcios/3/paradas/' + p + '/servicios?horaIni=')
+	// );
 
 	// let promiseHorarios = await Promise.all(urlHorarios);
 	// let jsonHorarios = await Promise.all(promiseHorarios.map((p) => p.json()));
