f2bd22dbae7a87088ed2b59154c8264357e0ed9d
Jesús Enrique Rascón <blograso@gmail.com>
2023-01-01 12:05:40 +0100

    working animations!

--- src/lib/Components/FavLineElement.svelte
+++ src/lib/Components/FavLineElement.svelte
@@ -13,8 +13,14 @@
 <div class="">
 	<div class="space-y-1">
 		<div class="flex justify-between items-center">
-			<div class="font-semibold text-2xl text-purple-900">
-				{paradaInfo.parada.nombre}
+			<div class="flex items-baseline space-x-2">
+				<div class="font-semibold text-purple-800 font-mono">
+					{paradaInfo.parada.orden}
+				</div>
+				<span class="w-1 h-1 self-center bg-purple-400 rounded-full" />
+				<div class="font-semibold text-2xl text-purple-900">
+					{paradaInfo.parada.nombre}
+				</div>
 			</div>
 			<button
 				class="text-blue-900"
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,26 +1,65 @@
 <script>
-	import { Star } from 'phosphor-svelte';
 	import FavLineElement from '../lib/Components/FavLineElement.svelte';
 	import LineElement from '../lib/Components/LineElement.svelte';
+
+	import { cubicOut } from 'svelte/easing';
+	import { crossfade } from 'svelte/transition';
+	import { flip } from 'svelte/animate';
+	import { Clock } from 'phosphor-svelte';
+
+	const [send, receive] = crossfade({
+		duration: (d) => Math.sqrt(d * 200),
+
+		fallback(node, params) {
+			const style = getComputedStyle(node);
+			const transform = style.transform === 'none' ? '' : style.transform;
+
+			return {
+				duration: 600,
+				easing: cubicOut,
+				css: (t) => `
+					transform: ${transform} scale(${t});
+					opacity: ${t}
+				`
+			};
+		}
+	});
 
 	export let data;
 	let paradasInfo = data.paradasInfo;
 	$: console.log(paradasInfo);
+
+	console.log(localStorage);
 </script>
 
 <div class="my-10">
 	<div class="text-6xl font-semibold tracking-tight text-blue-900">ELMETRO©</div>
-	<div class="text-xl font-medium tracking-tight text-blue-900">corre que lo pierdes!!</div>
+</div>
+
+<div class="items-baseline space-x-1 mb-9 text-lg">
+	<div class="flex space-x-1 items-center">
+		<span class="text-blue-900"><Clock weight="bold" /></span>
+		<div>Actualizado por última vez a las:</div>
+	</div>
+	<span class="font-mono font-bold text-blue-900 bg-blue-100 px-3 py-1 rounded-sm"
+		>{new Date().toLocaleTimeString()}</span
+	>
 </div>
 
 <!-- fav lines  -->
-<div class="text-3xl font-bold text-blue-900 flex items-center">
-	<span class="text-2xl mr-2">⭐</span> Favoritos
+<div class="text-2xl font-semibold text-blue-900 flex items-center mb-3">
+	<span class="text-2xl mr-2">⭐</span>Favs
 </div>
 <div class="">
 	<div class="font-light text-xl relative space-y-4">
 		{#each paradasInfo.filter((p) => p.favourite) as paradaInfo, i (paradaInfo.parada.orden)}
-			<FavLineElement bind:value={paradasInfo} {paradaInfo} />
+			<div
+				in:receive={{ key: paradaInfo.parada.orden }}
+				out:send={{ key: paradaInfo.parada.orden }}
+				animate:flip={{ duration: 100 }}
+			>
+				<FavLineElement bind:value={paradasInfo} {paradaInfo} />
+			</div>
 		{/each}
 	</div>
 </div>
@@ -32,6 +71,12 @@
 
 <div class="font-light text-xl relative border-l-4 border-dotted border-neutral-300 space-y-7">
 	{#each paradasInfo.filter((p) => !p.favourite) as paradaInfo, i (paradaInfo.parada.orden)}
-		<LineElement bind:value={paradasInfo} {paradaInfo} />
+		<div
+			in:receive={{ key: paradaInfo.parada.orden }}
+			out:send={{ key: paradaInfo.parada.orden }}
+			animate:flip={{ duration: 100 }}
+		>
+			<LineElement bind:value={paradasInfo} {paradaInfo} />
+		</div>
 	{/each}
 </div>
