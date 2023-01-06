f2df2dc829f2fc032b118b2203c63a076f884087
Jesús Enrique Rascón <blograso@gmail.com>
2023-01-06 17:45:27 +0100

    fix stupid thing because i am dumb

--- src/lib/Components/DisplayHourFav.svelte
+++ src/lib/Components/DisplayHourFav.svelte
@@ -1,20 +1,17 @@
 <script>
-	import { ArrowUp, ArrowDown } from 'phosphor-svelte';
 	import { dateFromHour } from '../utils';
 
 	import MinutesLeft from './MinutesLeft.svelte';
 	export let serviceInfo;
 
-	let minsIda = new Date(dateFromHour(serviceInfo.servicios[0].servicio) - new Date()).getMinutes();
-	let minsVuelta = new Date(
-		dateFromHour(serviceInfo.servicios[1].servicio) - new Date()
-	).getMinutes();
-	let minsIdaNext = new Date(
-		dateFromHour(serviceInfo.servicios[2].servicio) - new Date()
-	).getMinutes();
-	let minsVueltaNext = new Date(
-		dateFromHour(serviceInfo.servicios[3].servicio) - new Date()
-	).getMinutes();
+	let ida = serviceInfo.servicios.filter((s) => s.sentido == 1);
+	let vuelta = serviceInfo.servicios.filter((s) => s.sentido == 2);
+
+	let minsArmilla = new Date(dateFromHour(ida[0].servicio) - new Date()).getMinutes();
+	let minsArmillaNext = new Date(dateFromHour(ida[1].servicio) - new Date()).getMinutes();
+
+	let minsAlbolote = new Date(dateFromHour(vuelta[0].servicio) - new Date()).getMinutes();
+	let minsAlboloteNext = new Date(dateFromHour(vuelta[1].servicio) - new Date()).getMinutes();
 </script>
 
 <div>
@@ -22,17 +19,17 @@
 		<div
 			class="flex flex-col w-1/2 px-4 py-2 text-violet-900 bg-violet-100 bg-opacity-60 rounded-sm ring-1 ring-violet-600"
 		>
-			<MinutesLeft minutesLeft={minsIda} actualHour={serviceInfo.servicios[0].servicio} />
+			<MinutesLeft minutesLeft={minsAlbolote} actualHour={vuelta[0].servicio} />
 			<div class="opacity-40">
-				<MinutesLeft minutesLeft={minsIdaNext} actualHour={serviceInfo.servicios[2].servicio} />
+				<MinutesLeft minutesLeft={minsAlboloteNext} actualHour={vuelta[1].servicio} />
 			</div>
 		</div>
 		<div
 			class="flex flex-col w-1/2 px-4 py-2 bg-orange-100 rounded-sm ring-1 ring-orange-600 text-orange-900"
 		>
-			<MinutesLeft minutesLeft={minsVuelta} actualHour={serviceInfo.servicios[1].servicio} />
+			<MinutesLeft minutesLeft={minsArmilla} actualHour={ida[0].servicio} />
 			<div class="opacity-40 text-xl">
-				<MinutesLeft minutesLeft={minsVueltaNext} actualHour={serviceInfo.servicios[3].servicio} />
+				<MinutesLeft minutesLeft={minsArmillaNext} actualHour={ida[1].servicio} />
 			</div>
 		</div>
 	</div>
